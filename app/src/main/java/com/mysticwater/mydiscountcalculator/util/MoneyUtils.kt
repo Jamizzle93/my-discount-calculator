217c41f20bf142e7f961e955fa3e9623bd44d5aa
James Warner <james.warner@raspberrysoftware.com>
2018-08-12 10:21:23 +0100

    feat(MoneyUtils): Implement a method to format a string to '0.00'

--- app/src/main/java/com/mysticwater/mydiscountcalculator/util/MoneyUtils.kt
+++ app/src/main/java/com/mysticwater/mydiscountcalculator/util/MoneyUtils.kt
@@ -3,11 +3,27 @@ package com.mysticwater.mydiscountcalculator.util
 class MoneyUtils {
 
     companion object {
-        fun getSmallestDenomination(currencyText: String): Long {
+        fun getSmallestDenomination(moneyText: String): Long {
+            val fullMoneyString = formatStringToFullMoney(moneyText)
+
             val numbersRegex = Regex("[^0-9 ]")
-            val numbers = numbersRegex.replace(currencyText, "")
+            val numbers = numbersRegex.replace(fullMoneyString, "")
 
             return numbers.toLong()
+        }
+
+        private fun formatStringToFullMoney(moneyString: String): String {
+            val decimalIndex = moneyString.indexOf(".")
+            return if (decimalIndex == -1 || decimalIndex == (moneyString.length - 1)) {
+                "$moneyString.00"
+            } else {
+                val afterDecimalStr = moneyString.substring(decimalIndex + 1)
+                if (afterDecimalStr.length == 1) {
+                    moneyString + "0"
+                } else {
+                    moneyString
+                }
+            }
         }
     }
 }
